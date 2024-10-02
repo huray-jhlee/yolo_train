58fcb12fc43a25ddfcdbeebb1bf66c5a642c5a70
huray-jhlee <jh.lee@huray.net>
2024-10-02 17:43:49 +0900

    FEAT: add wandb logging

--- train.py
+++ train.py
@@ -1,16 +1,37 @@
+import wandb
+from wandb.integration.ultralytics import add_wandb_callback
+
 from ultralytics import YOLO
 
+
 def train(args):
-    model = YOLO("./yolov8n.pt")
+    
+    wandb.init(project="food_detector",
+               config={
+                   "epochs": args.epochs,
+                   "batch_size": args.batch,
+                   "resolution": args.imgsz,
+                   "gpus": args.gpus
+               })
+    
+    model = YOLO("./models/yolov8n.pt")
+    
+    add_wandb_callback(model, enable_model_checkpointing=True)
+    
     train_results = model.train(
         data=args.data,
         epochs=args.epochs,
         imgsz=args.imgsz,
-        device="0",
-        cache=False
+        device=args.gpus,
+        cache=False if args.cache is None else args.cache,
+        save_period=1,
+        workers=args.workers,
+        project="food_detector"
     )
     
     metrics = model.val()
+    
+    wandb.finish()
 
 if __name__ == "__main__":
     import argparse
@@ -18,7 +39,11 @@ if __name__ == "__main__":
     parser.add_argument("--epochs", type=int, default=10)
     parser.add_argument("--batch", type=int, default=64)
     parser.add_argument("--imgsz", type=int, default=640)
-    parser.add_argument("--data", type=str, default="/data/jh/detection_dataset/240924_dataset/data.yaml")
+    parser.add_argument("--data", type=str, default="/data/food_detector_dataset/food_dataset0927/food.yaml")
+    parser.add_argument("--cache", type=str, default=None)
+    parser.add_argument("--gpus", type=str)
+    parser.add_argument("--workers", type=int, default=8)
     args = parser.parse_args()
+    print(args.cache)
     
     train(args)
